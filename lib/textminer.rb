bcafd13ce99534512291bba0b0ec20a85578c063
Scott Chamberlain <myrmecocystus@gmail.com>
2015-08-22 08:48:29 -0700

    tabs to spaces

--- lib/textminer.rb
+++ lib/textminer.rb
@@ -3,26 +3,26 @@ require 'httparty'
 require 'json'
 
 module Textminer
-	##
-	# Examples:
-	#     require 'textminer'
-	#     # link to full text available
-	#     Textminer.links("10.5555/515151")
-	#     # no link to full text available
-	#     Textminer.links("10.1371/journal.pone.0000308")
-	def self.links(doi, type = 'xml')
-	  Request.new(doi).perform
+  ##
+  # Examples:
+  #     require 'textminer'
+  #     # link to full text available
+  #     Textminer.links("10.5555/515151")
+  #     # no link to full text available
+  #     Textminer.links("10.1371/journal.pone.0000308")
+  def self.links(doi, type = 'xml')
+    Request.new(doi).perform
   end
 
   class Request #:nodoc:
-  	attr_accessor :doi
+    attr_accessor :doi
 
     def initialize(doi)
       self.doi = doi
     end
 
     def perform
-    	url = "http://api.crossref.org/works/"
+      url = "http://api.crossref.org/works/"
       res = HTTParty.get(url + self.doi)
       Response.new(self.doi, res)
     end
@@ -49,18 +49,18 @@ module Textminer
     end
 
     def pdf
-    	tmp = links
-    	if !tmp.nil?
-    		tmp.select{|x| x['content-type'] == "application/pdf"}[0]['URL']
-    	end
+      tmp = links
+      if !tmp.nil?
+        tmp.select{|x| x['content-type'] == "application/pdf"}[0]['URL']
+      end
     end
 
     def xml
-    	tmp = links
-    	if !tmp.nil?
-    		tmp.select{|x| x['content-type'] == "application/xml"}[0]['URL']
-    	end
+      tmp = links
+      if !tmp.nil?
+        tmp.select{|x| x['content-type'] == "application/xml"}[0]['URL']
+      end
     end
-	end
+  end
 
 end
